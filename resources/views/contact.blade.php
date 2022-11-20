aa661cdc5b49b5e1bc7789ff88f12c3ca7edabb4
mahi <mahendran@webzeee.com>
2022-11-20 23:26:37 +0530

    completed contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,15 @@
 </head>
 <body>
     <h1>About Contact</h1>
-
+    <span>Let’s Create a Flexible website for your business.</span>
+    <form>
+        <table>
+            <tr>
+                <td>
+                    <label>Name</label>
+                </td>
+</tr>
+</table>
+</form>
 </body>
 </html>
